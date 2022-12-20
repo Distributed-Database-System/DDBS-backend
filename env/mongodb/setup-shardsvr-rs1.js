5c78383c8712083be41049dee6163e8578c30221
spricoder <spricoder@qq.com>
2022-12-20 21:10:25 +0800

    Remove env

--- env/mongodb/setup-shardsvr-rs1.js
+++
@@ -1,9 +0,0 @@
-rs.initiate(
-  {
-    _id: "shardsvr_rs1",
-    members: [
-      { _id: 0, host: "shardsvr1:27018" },
-      { _id: 1, host: "shardsvr2:27018" }
-    ]
-  }
-);
--- env/mongodb/setup-shardsvr-rs2.js
+++
@@ -1,9 +0,0 @@
-rs.initiate(
-  {
-    _id: "shardsvr_rs2",
-    members: [
-      { _id: 0, host: "shardsvr3:27018" },
-      { _id: 1, host: "shardsvr4:27018" }
-    ]
-  }
-);
